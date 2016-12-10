6732d11990773d9313f31387f0d5795606612055
zdoonio <dominik.zdunczyk@gmail.com>
2016-12-10 01:32:56 +0000

    ServerINIT

--- src/com/standard/Server.java
+++ src/com/standard/Server.java
@@ -1,5 +1,135 @@
 package com.standard;
 
-public class Server {
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+public class Server {
+	
+	// a unique ID for each connection
+	private static int uniqueId;
+	// an ArrayList to keep the list of the Client
+	private ArrayList<ClientThread> al;
+	// the port number to listen for connection
+	private int port;
+	// the boolean that will be turned of to stop the server
+	private boolean keepGoing;
+	// to display time
+	private SimpleDateFormat sdf;
+	
+	public Server(int port) {
+		// the port
+		this.port = port;
+		// to display hh:mm:ss
+		sdf = new SimpleDateFormat("HH:mm:ss");
+		// ArrayList for the Client list
+		al = new ArrayList<ClientThread>();
+	}
+	
+	public void start() {
+		keepGoing = true;
+		/* create socket server and wait for connection requests */
+		try 
+		{
+			// the socket used by the server
+			ServerSocket serverSocket = new ServerSocket(port);
+
+			// infinite loop to wait for connections
+			while(keepGoing) 
+			{
+				// format message saying we are waiting
+				//display("Server waiting for Clients on port " + port + ".");
+				
+				Socket socket = serverSocket.accept();  	// accept connection
+				// if I was asked to stop
+				if(!keepGoing)
+					break;
+				ClientThread t = new ClientThread(socket);  // make a thread of it
+				al.add(t);									// save it in the ArrayList
+				t.start();
+			}
+			// I was asked to stop
+			try {
+				serverSocket.close();
+				for(int i = 0; i < al.size(); ++i) {
+					ClientThread tc = al.get(i);
+					try {
+					tc.sInput.close();
+					tc.sOutput.close();
+					tc.socket.close();
+					}
+					catch(IOException ioE) {
+						// not much I can do
+					}
+				}
+			}
+			catch(Exception e) {
+				//display("Exception closing the server and clients: " + e);
+			}
+		}
+		// something went bad
+		catch (IOException e) {
+            String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+			//display(msg);
+		}
+	}		
+    /*
+     * For the GUI to stop the server
+     */
+	protected void stop() {
+		keepGoing = false;
+		// connect to myself as Client to exit statement 
+		// Socket socket = serverSocket.accept();
+		try {
+			new Socket("localhost", port);
+		}
+		catch(Exception e) {
+			// nothing I can really do
+		}
+	}
+	
+	// for a client who logoff using the LOGOUT message
+	synchronized void remove(int id) {
+			// scan the array list until we found the Id
+			for(int i = 0; i < al.size(); ++i) {
+				ClientThread ct = al.get(i);
+				// found it
+				if(ct.id == id) {
+					al.remove(i);
+					return;
+				}
+			}
+	
+	}
+	class ClientThread extends Thread {
+		// the socket where to listen/talk
+		Socket socket;
+		ObjectInputStream sInput;
+		ObjectOutputStream sOutput;
+		// my unique id (easier for deconnection)
+		int id;
+		// the Username of the Client
+		String username;
+		// the only type of message a will receive
+		ChatMessage cm;
+		// the date I connect
+		String date;
+
+		// Constructore
+		ClientThread(Socket socket) {
+			// a unique id
+			id = ++uniqueId;
+			this.socket = socket;
+			/* Creating both Data Stream */
+			System.out.println("Thread trying to create Object Input/Output Streams");
+		}
+	}
 }
